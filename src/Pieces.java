b732fab6bf8082e88139e6ddf92aab186e2325c9
Jiaweichen05 <JiaweiC11@nycstudents.net>
2022-05-11 10:26:36 -0400

    added stuff to Pieces.java

--- src/Pieces.java
+++ src/Pieces.java
@@ -1,2 +1,33 @@
-public class Pieces {
+public abstract class Pieces {
+    private boolean white = false;
+    private boolean killed = false;
+
+    public Piece(boolean white)
+    {
+        this.setWhite(white);
+    }
+
+    public boolean isWhite()
+    {
+        return this.white;
+    }
+
+    public void setWhite(boolean white)
+    {
+        this.white = white;
+    }
+
+    public boolean isKilled()
+    {
+        return this.killed;
+    }
+
+    public void setKilled(boolean killed)
+    {
+        this.killed = killed;
+    }
+
+    public abstract boolean canMove(Board board,
+                                    Spot start, Spot end);
+}
 }
