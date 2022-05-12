bf3450aa42b0d2ca2303d9acb3fd5c0641593347
BT_3W2_23 <aarons114@nycstudents.net>
2022-05-12 10:40:02 -0400

    update

--- src/GUI.java
+++ src/GUI.java
@@ -5,10 +5,17 @@ public class GUI extends JFrame{
     private JPanel panel;
     private JButton[][] buttons;
     private Spot[][] spots;
+    private Board board;
 
     public GUI(){
+        panel = new JPanel(new GridLayout(8,8));
         buttons = new JButton[8][8];
-        spots = new Spot[8][8];
-        
+        board = new Board();
+        for (int i = 0;i < buttons.length;i++){
+            for (int j = 0;j<buttons[0].length;j++){
+                panel.add(buttons[i][j]);
+            }
+        }
+        add(panel);
     }
 }
